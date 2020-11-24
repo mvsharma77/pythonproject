960d9742b9990c00a801dcd3358cacf9b77ba3ed
mvsharma77 <37452362+mvsharma77@users.noreply.github.com>
2020-11-24 19:41:23 +0100

    Update flask-app.py

--- Multiclass_model-master/src/flask-app.py
+++ Multiclass_model-master/src/flask-app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def runpipeline():
     return "ran the job"
 
+
 if __name__ == "__main__":
-app.run()
+    app.run(port=5000)
